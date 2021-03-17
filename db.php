7ad6177add0c1e2238f02d06a8d21c956235a69b
IssacPachecoPerez <80860507+IssacPachecoPerez@users.noreply.github.com>
2021-03-17 17:04:02 -0600

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $usuariodb = "root";
 $contrasenadb = "";
-$dbname = "registro-asv";
+$dbname = "";
 	
 // Generar conexion con el servidor MySQl
 $conexion = mysqli_connect($hostname, $usuariodb, $contrasenadb, $dbname);
